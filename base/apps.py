aa050dbd62edfd3aacbc5a616237c40602093804
Nimitha31 <nimithasakthivel3124@gmail.com>
2026-02-18 15:15:32 +0530

    Fix env var names for auto superuser

--- base/apps.py
+++ base/apps.py
@@ -1,5 +1,28 @@
 from django.apps import AppConfig
+import os
 
 
 class BaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
+
+    def ready(self):
+        if os.environ.get("CREATE_SUPERUSER") == "True":
+            try:
+                from django.contrib.auth import get_user_model
+                User = get_user_model()
+
+                username = os.environ.get("DJANGO_SU_NAME")
+                email = os.environ.get("DJANGO_SU_EMAIL")
+                password = os.environ.get("DJANGO_SU_PASSWORD")
+
+                if username and password:
+                    if not User.objects.filter(username=username).exists():
+                        User.objects.create_superuser(
+                            username=username,
+                            email=email,
+                            password=password,
+                        )
+                        print("✅ Superuser created")
+            except Exception as e:
+                print("❌ Superuser creation skipped:", e)
